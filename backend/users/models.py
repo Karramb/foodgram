ec1d787aa52ba5c8ac48632ea59d6b52424ff902
karramb <ap.vecna@gmail.com>
2025-02-18 18:58:15 +0300

    check with trainer

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from rest_framework import serializers
 from users.constants import MAX_LENGTH_FOR_EMAIL, MAX_LENGTH_FOR_FIELDS
 from users.validators import validate_username
 
