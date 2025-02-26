64d501cd7cabb8842f745ba2e64ef00ad5ad0457
karramb <ap.vecna@gmail.com>
2025-02-26 21:57:19 +0300

    fix short_url

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404, redirect
 from django.views.decorators.http import require_GET
-from rest_framework import status
+from rest_framework.reverse import reverse
 
 from recipes.models import Recipe
 
 
 @require_GET
 def short_url(request, pk):
-    if not Recipe.objects.filter(pk=pk).exists():
-        raise status.Http404(f'Рецепт с  id "{pk}"  не существует.')
-    return redirect(f'/recipes/{pk}/')
+    get_object_or_404(Recipe, pk=pk)
+    return redirect(reverse('recipes', args=[pk]))
