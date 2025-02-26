690c31acf227501ce7577765adc8a19a415f6bed
karramb <ap.vecna@gmail.com>
2025-02-26 22:56:48 +0300

    fix short_url

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -4,12 +4,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 from api.urls import urls as api_urls
-from api.views import short_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(api_urls)),
-    path('s/<int:pk>/', short_url, name='short_url')
 ]
 
 if settings.DEBUG:
